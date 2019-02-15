be56aa4ac825d1b8aa613b485cc1bdb38ce491b5
Rasolofoniaina <rasolofoniainaranto@gmail.com>
2019-02-15 18:32:53 +0300

    updates to deploy to heroku

--- serveur.js
+++ serveur.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/banquecloudfront/'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/banquecloudfront/index.html'));
+res.sendFile(path.join(__dirname + '/dist/banquecloudfront/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
